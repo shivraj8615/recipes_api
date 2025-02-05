30b5db2c1b5f5715dc63a6f555226f9c3a6346c2
shivraj8615 <75648551+shivraj8615@users.noreply.github.com>
2025-02-05 12:27:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ def get_recipe():
     shade = request.args.get('shade')  # Get shade from query params
     
     # Check if fabric type and shade exist in the recipes dictionary
-    if fabric_type == "cotton" and shade== "dark":
+    if fabric_type == "Cotton" and shade== "dark":
         recipe = recipes_1_cotton_dark
         return jsonify(recipe)
-    if fabric_type == "cotton" and shade== "medium":
+    if fabric_type == "Cotton" and shade== "medium":
         recipe = recipes_2_cotton_medium
         return jsonify(recipe)
-    if fabric_type == "cotton" and shade== "light":
+    if fabric_type == "Cotton" and shade== "light":
         recipe = recipes_3_cotton_light
         return jsonify(recipe)
       # Return the recipe as a JSON response
-    if fabric_type == "polyster" and  shade=="dark":
+    if fabric_type == "Polyster" and  shade=="dark":
         recipe = recipes_4_polyester_dark
         return jsonify(recipe)
-    if fabric_type == "polycotton" and shade == "dark":
+    if fabric_type == "Polycotton" and shade == "dark":
         recipe = recipes_5_pc_dark
         return jsonify(recipe)
     else:
